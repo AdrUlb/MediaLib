8931c12886fd034cc7591e40a512bed4f606ce27
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2020-12-23 00:03:07 +0100

    Update submodules

--- MediaLib/Window.cs
+++ MediaLib/Window.cs
@@ -1,10 +1,25 @@
-﻿namespace MediaLib
+﻿using ManagedSDL2;
+using System;
+
+namespace MediaLib
 {
 	public class Window
 	{
+		SDLWindow sdlWindow;
+
 		public Window()
 		{
+			SDL.Init(SDLInitFlags.Video);
+			sdlWindow = new SDLWindow("", SDL.WindowPosUndefined, SDL.WindowPosUndefined, 400, 400, true);
+		}
 
+		~Window() => Dispose();
+
+		private void Dispose()
+		{
+			Console.WriteLine("hi");
+			GC.SuppressFinalize(this);
+			sdlWindow.Dispose();
 		}
 	}
 }
