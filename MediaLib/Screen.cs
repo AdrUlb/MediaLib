3a971268634d92ccec6cadd6d54efcac343d1ad2
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2021-01-12 17:20:55 +0100

    Added events to screen

--- MediaLib/Screen.cs
+++ MediaLib/Screen.cs
@@ -9,6 +9,16 @@ namespace MediaLib
 		List<Element> elements = new List<Element>();
 		internal Window? Window;
 
+		public event ShowEventHandler? Show;
+		public event HideEventHandler? Hide;
+		public event UpdateEventHandler? Update;
+		public event DrawEventHandler? Draw;
+
+		protected virtual void OnShow() { }
+		protected virtual void OnHide() { }
+		protected virtual void OnUpdate(UpdateEventArgs e) { }
+		protected virtual void OnDraw() { }
+
 		public void AddElement(Element element)
 		{
 			if (element.Screen != null)
@@ -38,24 +48,38 @@ namespace MediaLib
 
 		internal void HandleShow()
 		{
+			Show?.Invoke();
+			OnShow();
+
 			foreach (var element in elements)
 				element.HandleShow();
 		}
 
 		internal void HandleHide()
 		{
+			Hide?.Invoke();
+			OnHide();
+
 			foreach (var element in elements)
 				element.HandleHide();
 		}
 
 		internal void HandleUpdate(TimeSpan delta)
 		{
+			var e = new UpdateEventArgs(delta);
+
+			Update?.Invoke(e);
+			OnUpdate(e);
+
 			foreach (var element in elements)
-				element.HandleUpdate(delta);
+				element.HandleUpdate(delta, e);
 		}
 
 		internal void HandleDraw()
 		{
+			Draw?.Invoke();
+			OnDraw();
+
 			foreach (var element in elements)
 				element.HandleDraw();
 		}
